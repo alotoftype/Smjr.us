5d406c4104889b50c4f2fdf503ba452fdc20ed47
SMjr <alotoftype@users.noreply.github.com>
2019-06-16 11:41:00 -0400

    Adding footer

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Serge M. Portfolio Website`,
+    description: `Portfolio website using Gatsby`,
+    author: `Serge M.`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
